d1983b8780aa259c8dad58e78a49f1fff0944462
maedehjavadi <missmadeo1997@gmail.com>
2022-01-19 23:51:30 +0330

    guess word

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ let guessed=[]
 let hiddenWord=null
 
 
-function randWord(params) {
+function randWord() {
     answer=dictionary[Math.floor(Math.random()*dictionary.length)]
 }
 function generateAlphabet() {
@@ -18,6 +18,24 @@ function generateAlphabet() {
 
     document.getElementById('keyboard').innerHTML=alphabet
 }
+function handleGuess(chosenLetter) {
+    guessed.indexOf(chosenLetter) === -1 ? guessed.push(chosenLetter) : null;
+    document.getElementById(chosenLetter).setAttribute('disabled', true);
+  
+    if (answer.indexOf(chosenLetter) >= 0) {
+      guessedWord();
+      checkIfGameWon();
+    } else if (answer.indexOf(chosenLetter) === -1) {
+      mistakes++;
+      
+    }
+  }
+  function checkIfGameWon() {
+    if (wordStatus === answer) {
+      document.getElementById('keyboard').innerHTML = 'You Won!!!';
+    }
+  }
+  
 
 function guessedWord() {
     hiddenWord = answer.split('').map(letter=>(guessed.indexOf(letter) >= 0? letter:"_")).join(' ');
